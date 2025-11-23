f72567fc1ba79e2ee98cf9b4bb01d19a7e4f9b8e
Kabir <mohammadtansimkabir547@yahoo.com>
2025-11-23 23:28:42 +0000

    Add files via upload

---
+++ Java/Basics/WhileLoops.java
@@ -0,0 +1,65 @@
+//import java.util.Scanner;
+//import java.util.ArrayList;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+
+		//int a = 0;
+
+		/*while(a < 10){
+		System.out.println("a is less than 10: " + a);	
+			a++;						
+		}*/
+
+		/*do { 
+
+			System.out.println("a");
+		}while(a > 9); {
+
+			System.out.println("a is equals to 1: " + a);
+		a++;
+		}*/
+
+		/*	String sentence = "flapjacks are awesome";
+		Scanner myscanner = new Scanner(sentence);
+		ArrayList<String> words = new ArrayList<String>();
+
+		while(myscanner.hasNext()) {
+			words.add(myscanner.next());					
+		}
+		System.out.println(words);
+		 */		
+
+		/*int i = 1;
+
+		while(i < 6) {
+			System.out.println("Count is: " +i); 
+			i++;
+		}*/
+
+		/*int i = 1;
+
+		do {
+			System.out.println("Count is: " + i);
+			i++;
+		}
+		while (i<6);*/
+
+		/*for(int i = 0; i<10; i++) {
+			System.out.println("oola la");			
+		}*/
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
